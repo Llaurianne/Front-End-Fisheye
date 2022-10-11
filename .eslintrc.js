e9f2761aed4ae3e7191d4d59fae63dc139506840
Laurianne <valette.laurianne@gmail.com>
2022-10-11 13:21:21 +0200

    Eslint globals and rules, style corrections on contact form

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,17 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "no-unused-vars": "off"
+    },
+    "globals": {
+        "main": "readonly",
+        "mainHeader": "readonly",
+        "manageContactForm": "readonly",
+        "manageLightbox": "readonly",
+        "mediaDOMArray": "readonly",
+        "mediaFactory": "readonly",
+        "module": "readonly",
+        "photographerFactory": "readonly",
+        "photographerDatas": "readonly"
     }
 }
